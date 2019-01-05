e85d60fd03326489f63ea8317b47c314ebbfa690
gigy410 <36370033+gigy410@users.noreply.github.com>
2019-01-05 11:58:32 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,49 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css">
+        <script src="https://code.jquery.com/jquery-3.3.1.min.js" 
+        integrity="sha384-tsQFqpEReu7ZLhBV2VZlAu7zcOV+rXbYlF2cqB8txI/8aZajjp4Bqd+V6D5IgvKT" 
+        crossorigin="anonymous"></script>
+        <script src="JS/function.js" ></script>
+        
+        <link rel="stylesheet" type="text/css" href="CSS/style.css">
+    </head>
+    <body>
+        <table class="tabella">
+            <tr>
+                <td>
+                    <div id="box1" class="square">
+                        <div class="content">
+                            <div id="nome1"></div>
+                            <div id="valore1"></div>
+                        </div>
+                    </div>
+                </td>
+                <td>
+                    <div id="box2" class="square">
+                        <div class="content">
+                            <div id="nome2"></div>
+                            <div id="valore2"></div>
+                        </div>
+                    </div>
+                </td>
+                <td>
+                    <div id="box3" class="square">
+                        <div class="content">
+                            <div id="nome3"></div>
+                            <div id="valore3"></div>
+                        </div>
+                    </div>
+                </td>
+                <td>
+                    <div id="box4" class="square">
+                        <div class="content">
+                            <div id="nome4"></div>
+                            <div id="valore4"></div>
+                        </div>
+                    </div>
+                </td>
+            </tr>
+        </table>
+    </body>
+</html>
