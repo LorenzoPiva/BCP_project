e64a78031f0c6705eab3ab9078849e3d35e5ced6
Luigi Di Benedetto - account scolastico <36370033+gigy410@users.noreply.github.com>
2019-01-16 22:34:03 +0100

    aggiorno l'html

--- index.php
+++ index.php
@@ -1,50 +1,79 @@
 <html>
     <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css">
-        <script src="https://code.jquery.com/jquery-3.3.1.min.js" 
-        integrity="sha384-tsQFqpEReu7ZLhBV2VZlAu7zcOV+rXbYlF2cqB8txI/8aZajjp4Bqd+V6D5IgvKT" 
-        crossorigin="anonymous"></script>
-        <script src="JS/function.js" ></script>
-        
         <link rel="stylesheet" type="text/css" href="CSS/style.css">
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+        <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha384-tsQFqpEReu7ZLhBV2VZlAu7zcOV+rXbYlF2cqB8txI/8aZajjp4Bqd+V6D5IgvKT" crossorigin="anonymous"></script>
+        <script src="JS/function.js" ></script>
     </head>
-    <body>
-        <table class="tabella">
-            <tr>
-                <td>
-                    <div id="box1" class="square">
-                        <div class="content">
-                            <div id="nome1"></div>
-                            <div id="valore1"></div>
+    <body style="background-color: #d1d1e0">
+        <nav class="navbar header-top fixed-top navbar-expand-lg  navbar-dark bg-dark">
+          <span></span>
+          <a class="navbar-brand" href="#"><img width="150px" src="IMG/logo.png"></a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText"
+            aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarText">
+            
+            <ul class="navbar-nav animate side-nav">
+              <li class="nav-item">
+                <a class="nav-link" href="#">Home
+                  <span class="sr-only">(current)</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">1</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">2</a>
+              </li>
+            </ul>
+            
+            <ul class="navbar-nav ml-md-auto d-md-flex">
+              <li class="nav-item">
+                <a class="nav-link" href="#">Home
+                  <span class="sr-only">(current)</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">API</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">TEAM</a>
+              </li>
+            </ul>
+            
+          </div>
+        </nav>
+        <div class="classifica">
+            <div class="Rtable Rtable--4cols">
+                        <div style="order:1;" id="box1" class="Rtable-cell square">
+                            <div class="content">
+                                <div id="nome1"></div>
+                                <div id="valore1"></div>
+                            </div>
                         </div>
-                    </div>
-                </td>
-                <td>
-                    <div id="box2" class="square">
-                        <div class="content">
-                            <div id="nome2"></div>
-                            <div id="valore2"></div>
+                        <div style="order:1;" id="box2" class="Rtable-cell square">
+                            <div class="content">
+                                <div id="nome2"></div>
+                                <div id="valore2"></div>
+                            </div>
                         </div>
-                    </div>
-                </td>
-                <td>
-                    <div id="box3" class="square">
-                        <div class="content">
-                            <div id="nome3"></div>
-                            <div id="valore3"></div>
+                        <div style="order:1;" id="box3" class="Rtable-cell square">
+                            <div class="content">
+                                <div id="nome3"></div>
+                                <div id="valore3"></div>
+                            </div>
                         </div>
-                    </div>
-                </td>
-                <td>
-                 
-                    <div id="box4" class="square">
-                        <div class="content">
-                            <div id="nome4"></div>
-                            <div id="valore4"></div>
+                        <div style="order:1;" id="box4" class="Rtable-cell square">
+                            <div class="content">
+                                <div id="nome4"></div>
+                                <div id="valore4"></div>
+                            </div>
                         </div>
-                    </div>
-                </td>
-            </tr>
-        </table>
+            </div>
+        </div>
     </body>
 </html>
